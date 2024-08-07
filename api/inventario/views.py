59a75c54d0c9581be7bb738b8f13bafa11035124
Kerpooo <kevinrincon8@gmail.com>
2024-08-07 01:13:25 -0500

    fix: Inventario lista
    La lista de inventarios ahora muestra el nombre del producto y el nombre de la bodega

--- api/inventario/views.py
+++ api/inventario/views.py
@@ -1,9 +1,26 @@
 from .models import Inventario
 from .serializer import InventarioSerializer
 from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 
 
 class InventarioViewSet(viewsets.ModelViewSet):
     queryset = Inventario.objects.all()
     serializer_class = InventarioSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def list(self, request, format=None):
+        queryset = self.get_queryset()
+        data = []
+        for item in queryset:
+            data.append(
+                {
+                    "id": item.id_inventario,
+                    "cantidad_disponible": item.cantidad_disponible,
+                    "nombre_producto": (
+                        item.id_producto.nombre if item.id_producto else None
+                    ),
+                    "nombre_bodega": item.id_bodega.nombre if item.id_bodega else None,
+                }
+            )
+        return Response(data)
